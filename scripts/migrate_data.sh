482fa9b1aa9899ccd2ab55988bae6e3bde1906dc
David M <david@smooth-systems.solutions>
2018-05-29 22:48:31 +0200

    updated data migration file

--- scripts/migrate_data.sh
+++ scripts/migrate_data.sh
@@ -11,8 +11,6 @@ DIST_DIR=${APP_DIR}/dist_new
 SRC_CONFIG=config/
 
 # application variables
-#APP_JAR_NAME=ecommerce-utils-1.0.0-SNAPSHOT.jar
-#APP_TARGET_DIR=app/ecommerce-utils/target
 APP_JAR_NAME=ecommerce-migration-tool-1.0.0-SNAPSHOT.jar
 APP_TARGET_DIR=app/ecommerce-migration-tool/target
 APP_JAR=${DIST_DIR}/${APP_JAR_NAME}
@@ -30,25 +28,18 @@ CONFIG_DIR=${DIST_DIR}/configs
 if [ ! -f ${APP_JAR} ]; then
 
   echo ""
-  echo "${INFO} Cleanup/Remove directory ${DIST_DIR}"
-  rm -rf ${DIST_DIR}
+  echo "${INFO} Create directory ${DIST_DIR}"
   mkdir -p ${DIST_DIR}
 
   echo "${INFO} Setup application jar file ${APP_JAR}"
   cp ${APP_TARGET_DIR}/${APP_JAR_NAME} ${DIST_DIR}/
 
-	rm -rf ${CONFIG_DIR}
 	mkdir -p ${CONFIG_DIR}
 	cp ${SRC_CONFIG}/migration-config.yaml ${CONFIG_DIR}
   echo "${INFO} Successfully copied and setup application configuration ..."
   echo ""
 fi
 
-
-
-# migrate categories ============================
-# -----------------------------------------------
-# java ecommerce-utils-1.0.0-SNAPSHOT.jar --merge-products --config=<PATH-TO-CONFIGURATION>/migration-config.yaml
 
 
 # migrate products ==============================
@@ -76,7 +67,10 @@ case "$1" in
     echo ""
     echo " [WARN] No command specified!"
     echo ""
-    echo "  Usage: ${0} merge-categories | merge-products | merge-product-images"
+    echo " # ########################################################################"
+    echo " # Main command for migration:"
+    echo ""
+    echo "   Usage: ${0} merge-categories | merge-products | merge-product-images"
     echo ""
     ;;
 esac
