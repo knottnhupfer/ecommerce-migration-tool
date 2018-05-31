abe925cd70f2d8699b0d37899945f7da78475b0b
David M <david@smooth-systems.solutions>
2018-05-31 13:28:51 +0200

    updated migration client, added reader interface

---
+++ app/migration-client-api/src/main/java/org/smooth/systems/ec/client/api/SimpleProduct.java
@@ -0,0 +1,15 @@
+package org.smooth.systems.ec.client.api;
+
+import lombok.Data;
+
+/**
+ * Created by David Monichi <david.monichi@smooth-systems.solutions> on
+ * 31.05.18.
+ */
+@Data
+public class SimpleProduct {
+
+  private Long productId;
+
+  private String langIso;
+}
